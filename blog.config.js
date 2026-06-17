c046505eb34bee6a6b69db7bd572a68a1c588bfe
Gomb <yourchum@gmail.com>
2026-06-17 22:20:04 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'dark', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || false, // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '魔剑中文站2026', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '魔剑中文站', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '魔剑萌新导引', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.shadowbane.online', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '魔剑,shadowbane,mmorpg,game,gaming', // 网站关键词 英文逗号隔开
